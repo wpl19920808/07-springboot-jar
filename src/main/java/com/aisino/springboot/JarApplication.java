f74409ed115d48ef3780035597f6ee7095acd2b7
anthony <565349501@qq.com>
2019-03-26 09:53:20 +0800

    springboot jar

---
+++ src/main/java/com/aisino/springboot/JarApplication.java
@@ -0,0 +1,12 @@
+package com.aisino.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JarApplication{
+
+    public static void main(String[] args) {
+        SpringApplication.run(JarApplication.class, args);
+    }
+}
---
+++ src/main/java/com/aisino/springboot/controller/WarController.java
@@ -0,0 +1,26 @@
+package com.aisino.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class WarController {
+    @RequestMapping("/boot/json")
+    public @ResponseBody Object json(){
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("name", "Anthony");
+        map.put("age", "14");
+        return map;
+    }
+
+    @RequestMapping("/boot/jsp")
+    public String jsp(Model model){
+        model.addAttribute("name", "航天信息");
+        return "index";
+    }
+}
